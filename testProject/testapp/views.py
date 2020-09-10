3292bafcd8f744763a0caccbc5917a63859482e1
smkgs96 <smkgs96@gmail.com>
2020-09-10 16:17:52 +0900

    메인화면.html to main.html

--- testProject/testapp/views.py
+++ testProject/testapp/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, 'testapp/메인화면.html', {})
+    return render(request, 'testapp/main.html', {})
 
 def safety(request):
     return render(request, 'testapp/sobanganjeon.html', {})
