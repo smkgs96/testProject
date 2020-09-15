3af6ded64f95c07cb88a6af2f512469a2c2344ad
Hyunjik Lee <jiggy0429@gmail.com>
2020-09-15 14:14:38 +0900

    Remove obsolete lines in views.py

--- testProject/testapp/views.py
+++ testProject/testapp/views.py
@@ -6,7 +6,7 @@ import requests
 
 def home(request):
     sensors = Sensor.objects.all().order_by('-id')[:5]
-    states = State.objects.all().order_by('-state_num')[:1]
+    states = State.objects.all().order_by('-state_num')[:5]
     probs = Probability.objects.all().order_by('-prob_id')[:1]
     context = {'sensors': sensors, 'states' : states, 'probs' : probs}
     return render(request, 'testapp/main.html', context=context)
@@ -18,14 +18,8 @@ def statistics(request):
     return render(request, 'testapp/statistics.html', {})
 
 def location(request):
-    r = requests.post('https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyBGE4LjBk3a2Lsqi1CzGHAYInoWWZTOsSo', params=request.POST)
+    return render(request, 'testapp/location.html', {})
     
-    if r.status_code == 200:
-        context = {'location': r.text}
-        return render(request, 'testapp/location.html', context=context)
-    else:
-        return render(request, 'testapp/location.html', {'location' : 'fail'})
-
 def setting(request):
     return render(request, 'testapp/setting.html', {})
 
